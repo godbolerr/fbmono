74cadeec64b0d1bd0576c2c14a4b7ac664fdce48
admin <admin@test.com>
2017-03-02 10:07:51 +0530

    initial commit

---
+++ src/main/java/com/work/fb/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.work.fb.config.audit;
---
+++ src/main/java/com/work/fb/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.work.fb.repository;
+
+import com.work.fb.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
